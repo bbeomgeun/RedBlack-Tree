fbe45733902bc4049f206b86bf84a74b2fd49865
bbeomgeun <miki308@naver.com>
2021-05-12 19:00:29 +0900

    Insert, Recoloring, 멤버변수(color),멤버함수(get,set) 추가

--- 12161104_beomgeunPark.cpp
+++ 12161104_beomgeunPark.cpp
@@ -6,13 +6,19 @@
 #include <vector>
 
 using namespace std;
+
+enum class Color { // 노드의 color를 red와 black으로 제한하기 위한 enum class
+	COLOR_RED,
+	COLOR_BLACK
+};
+
 // 원래 pair<int, int>와 pair<string, int>를 사용했지만 변수명을 직관적으로 사용하기 위해 클래스화 (first, second -> 각 이름)
-class Adress_XY { // 주소 좌표를 기록하기 위한 클래스
+class Address_XY { // 주소 좌표를 기록하기 위한 클래스
 public:
 	int x;
 	int y;
 
-	Adress_XY(int _x, int _y) {
+	Address_XY(int _x, int _y) {
 		this->x = _x;
 		this->y = _y;
 	}
@@ -34,12 +40,12 @@ public:
 	int id; // 환자번호 (기준키, 유일함): 1,000,000 ~ 1,999,999 사이의 정수. 
 	string name; // 이름 : 공백 없는 20 bytes 이내의 문자열.예) “Mary”
 	string phoneNumber; // 연락처 : 회원의 전화번호(공백 없는 11 bytes 이내의 문자열).예) “01012345678”
-	Adress_XY  * address; // 주소 좌표(x, y) : x와 y는 각각 0 ~10, 000 사이의 임의의 정수.
+	Address_XY  * address; // 주소 좌표(x, y) : x와 y는 각각 0 ~10, 000 사이의 임의의 정수.
 	Record  * record; //진료기록 : 아래 정보들의 나열 (	-병명 : 공백 없는 20 bytes 이내의 문자열.예) “Headache”, 진료비: 10, 000 ~100, 000 사이의 정수)
 	// 환자 정보를 담을 멤버 변수
 
 	Patient(int _id, string _name, string _phoneNumber, int _addressX, int _addressY, string _diseaseName, int _price) {
-		Adress_XY * _address_xy = new Adress_XY(_addressX, _addressY); // 클래스 객체 생성
+		Address_XY * _address_xy = new Address_XY(_addressX, _addressY); // 클래스 객체 생성
 		Record  * _record = new Record(_diseaseName, _price);// 클래스 객체 생성
 
 		this->id = _id;
@@ -56,22 +62,52 @@ public:
 	void setPrice(int _price) {
 		this->record->price = _price;
 	}
-
-
 };
 
 class Node {
 public:
-	Patient * patient;
-	Node* parent;
-	Node* left;
-	Node* right;
-	int depth;
+	Patient * patient; // 노드의 환자 정보
+	Node* parent; // 부모 노드
+	Node* left; // 왼쪽 노드
+	Node* right; // 오른쪽 노드
+	int depth; // 노드의 depth
+	Color color; // 노드의 color
 
-	Node(Patient * _data) {
+	Node(Patient * _data) { 
 		this->patient = _data;
-		parent = left = right = NULL;
+		parent =NULL;
 		depth = 0;
+		left->setColor(Color::COLOR_BLACK);
+		right->setColor(Color::COLOR_BLACK);
+		this->setColor(Color::COLOR_RED);
+	} // 새 노드 추가 시, 해당 환자정보, depth = 0, red color, 자식노드 black으로 생성
+
+	void setColor(Color c) {
+		this->color = c;
+	}
+
+	int getID() {
+		return this->patient->id;
+	}
+
+	Node* getParentNode() {
+		return this->parent;
+	}
+
+	Node* getUncleNode() { // 현재 노드에서 부모의 부모의 자식 노드를 리턴
+		Node* parentNode = this->parent;
+		Node* grandParent = parentNode->parent;
+
+		if (grandParent->left == parentNode) { // 만약 grand의 left과 부모이면 오른쪽이 uncleNode
+			return grandParent->right;
+		}
+		else { // grand의 right이 부모이면 왼쪽이 uncleNode
+			return grandParent->left;
+		}
+	}
+
+	Node* getGrandNode() {
+		return this->parent->parent;
 	}
 };
 
@@ -87,26 +123,79 @@ public:
 		return root;
 	}
 
-	void insert(Node* insert_node) {
+	bool isDoubleRed(Node* curNode) { // doubleRed 체크 (현재 노드와 부모 노드의 color가 모두 red면 true)
+		if (curNode == root) // 만약 root노드를 체크하려고 들어오면 오류가 나므로 false return
+			return false;
+
+		Node* parent = curNode->getParentNode();
+		if (curNode->color == Color::COLOR_RED && parent->color == Color::COLOR_RED)
+			return true;
+		else
+			return false;
+	}
+
+	bool isBlack(Node* curNode) { // 받은 node의 black 체크(블랙이면 true값)
+		return curNode->color == Color::COLOR_BLACK;
+	}
+
+	Node* recoloring(Node* curNode) { // 기준 노드 z의 parent, grand, uncle의 색을 바꾼다. 포인터 연산이므로 값이 변경됨
+		Node* parentNode = curNode->getParentNode();
+		Node* grandNode = curNode->getGrandNode();
+		Node* uncleNode = curNode->getUncleNode();
+
+		if (grandNode != root) { // 루트가 아닐 경우 red로 변경해준다. (루트는 무조건 블랙)
+			grandNode->setColor(Color::COLOR_RED);
+		}
+		uncleNode->setColor(Color::COLOR_BLACK);
+		parentNode->setColor(Color::COLOR_BLACK);
+
+		return grandNode; // grand가 red로 바뀌는 케이스이므로 grand와 grand의 parent의 red를 비교해야함
+	}
+
+	Node* insert(Node* insert_node) { // insertNode : 해당 환자정보, depth = 0, 자신 : red color, 자식 : black으로 생성
 		if (root == NULL) { // 빈 트리일 경우 root에 insert해준다.
 			root = insert_node;
+			root->setColor(Color::COLOR_BLACK); // root의 경우 black으로 바꿔준다, root는 depth와 parent 정보 바꿔줄 필요 없다.
+			return root; // 루트는 insert하고 종료(recoloring 필요 없음)
 		}
-		else { // root가 있다면 자신의 자리를 찾아서 insert
+		else { // root가 있다면 자신의 자리를 찾아서 insert 후 recoloring 진행
 			Node* curNode = root;
 			Node* upperNode = NULL;
-			bool findFlag = false;
 			while (curNode != NULL) {
 				upperNode = curNode;
-				if (curNode->patient->id == insert_node->patient->id) {
-					findFlag = true;
-					int depth = curNode->depth;
-					break;
+				if (curNode->getID() == insert_node->getID()) { // 만약 id가 중복된다면 insert 실패
+					return curNode; // 중복된 노드를 반환한다 (query_I에서 비교해서 리턴값이 insert하려는 것과 같으면 승인, 다른 정보면 실패
 				}
-				else if (curNode->patient->id < insert_node->patient->id) {
+				else if (curNode->getID() > insert_node->getID()) { // insert보다 현재 노드의 key가 더 크면 left로 간다
 					curNode = curNode->left;
 				}
-				else if (curNode->patient->id > insert_node->patient->id) {
+				else if (curNode->getID() < insert_node->getID()) { // 현재 노드의 key가 더 작으면 right으로 간다.
 					curNode = curNode->right;
+				}
+			} // while이 종료되면 upperNode의 자식 위치에 _insertNode가 삽입된다. left 또는 right를 정해주자, 현재 curNode는 null
+
+			if (upperNode->getID() > insert_node->getID()) {
+				upperNode->left = insert_node;
+			}
+			else if (upperNode->getID() < insert_node-> getID()) {
+				upperNode->right = insert_node;
+			}
+			insert_node->parent = upperNode;
+			insert_node->depth = upperNode->depth + 1;
+			// 현재 insert_node 부모연결, depth, patient, color - red, child - black 상태
+
+			Node * z = insert_node; // 기준노드 z에 insert 후 노드 관계가 완성된 insert_node를 저장
+
+			while (isDoubleRed(z)) { //복사해준 curNode를 기준으로 recoloring 시작
+				// 만약 z의 grand가 null인 경우의 예외처리를 하려했지만 그 경우는 z의 parent가 root(black)인 경우로 isDoubleRed가 무조건 false가 나온다.
+				if (isBlack((z->getUncleNode()))) { // uncle이 black이면 restructuring
+					//z = restructure(z)
+					return;
+				}
+				else  { // uncle red이면 recoloring
+					z = recoloring(z); 
+					// 노드 z의 parent, uncle, grand의 색을 바꿈, grand가 root일 경우 색을 바꾸지 않음, 리턴값은 z의 grand - grand가 red가 되므로 grand를 비교해야함
+					//따라서 z는 grand가 되며 grand가 root가 될수도 있음 -> isDoubleRed함수에 root 예외처리 추가
 				}
 			}
 		}
@@ -115,13 +204,13 @@ public:
 	Node* find(int find_id) { // red-black tree의 find는 binary search tree의 find와 동일하다.
 		Node* curNode = root;
 		while (curNode != NULL) {
-			if (curNode->patient->id == find_id) {
+			if (curNode->getID() == find_id) {
 				return curNode;
 			}
-			else if (curNode->patient->id < find_id) {
+			else if (curNode->getID() < find_id) {
 				curNode = curNode->left;
 			}
-			else if (curNode->patient->id > find_id) {
+			else if (curNode->getID() > find_id) {
 				curNode = curNode->right;
 			}
 		}
@@ -146,21 +235,23 @@ public:
 	}
 
 	void query_I(int _id, string _name, string _phoneNumber, int _addressX, int _addressY, string _diseaseName, int _price) {
+		Patient* newPatientInfo = new Patient (_id, _name, _phoneNumber, _addressX, _addressY, _diseaseName, _price);
+		Node* newNode = new Node(newPatientInfo);
+
+		Node* insertResultNode = insert(newNode);
+		if (insertResultNode->patient == newNode->patient) { // 정상 insert
+			cout << insertResultNode->depth << " " << 1 << "\n"; // 승인코드 1
+		}
+		else { // 이미 중복된 insert
+			cout << insertResultNode->depth << " " << 0 << "\n"; // 거절코드 0
+		}
 		// insert (K N H Ax Ay DI C)쿼리에 관한 내용
 		// parameter로 patient 객체 생성하고, 트리에 삽입하는 과정(red-black tree)
 		//print : 노드 depth와 신규가입처리여부 
 		//-질의형식: “I K N H Ax Ay DI C”
-		//	I : 환자를 신규 가입하는 질의를 나타내는 기호
-		//	K : 환자번호
-		//	N : 이름
-		//	H : 연락처
-		//	Ax : 주소의 x 좌표
-		//	Ay : 주소의 y 좌표
-		//	DI : 병명
-		//	C : 진료비
-		//	- 출력형식 : “D S”
-		//	D : 트리에서 환자 정보가 저장된 노드의 깊이(root의 깊이는 0)
-		//	S : 신규 가입 처리를 나타내는 정수(0 또는 1) (0: 거절, 1 : 승인)
+		//	I : 환자를 신규 가입하는 질의를 나타내는 기호		//	K : 환자번호		//	N : 이름		//	H : 연락처		//	Ax : 주소의 x 좌표		//	Ay : 주소의 y 좌표
+		//	DI : 병명		//	C : 진료비
+		//	- 출력형식 : “D S”		//	D : 트리에서 환자 정보가 저장된 노드의 깊이(root의 깊이는 0)		//	S : 신규 가입 처리를 나타내는 정수(0 또는 1) (0: 거절, 1 : 승인)
 		//- 설명 : 새로운 환자 정보를 입력받아 레드블랙트리에 노드를 삽입하고, 그 노드의 깊이를 출력한다.
 		//	만약 동일한 환자번호가 이미 존재한다면, 그 노드의 깊이를 출력하고 신규 가입은 거절된다.
 	}
