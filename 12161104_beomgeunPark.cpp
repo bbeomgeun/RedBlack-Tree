5f1474370acd8175b7fbe3b58527b73f9333bb86
bbeomgeun <miki308@naver.com>
2021-05-26 23:23:14 +0900

    Fix Tree Vertex wrong answer 원인 : 무조건 heap에서 pop할때마다 tree vertex를 올려주면 이미 tree가 된 정점이 우선순위를 갖고 또 pop될수 있음

--- 12161104_beomgeunPark.cpp
+++ 12161104_beomgeunPark.cpp
@@ -5,18 +5,13 @@
 #include <queue>
 #include <unordered_map>
 #include <stack>
+#include <algorithm>
 
 
 #define MAX 1000000 // 10^6
 #define INF 987654321
 
 using namespace std;
-
-enum class Status {
-	UNSEEN,
-	FRINGE,
-	TREE
-};
 
 class AreaInfo { // 지역정보 -> vertex
 public:
@@ -32,18 +27,22 @@ public:
 };
 
 class Graph { // 무향그래프
-	// 먼저 입력받는 지역 정보를 저장해둬야하고, 연결정보를 받으면 연결된 지역끼리 묶고
-	// 그 이후 dijkstra를 돌리면 된다(최단거리 및 최단경로)
+	 //먼저 입력받는 지역 정보를 저장해둬야하고, 연결정보를 받으면 연결된 지역끼리 묶고
+	 //그 이후 dijkstra를 돌리면 된다(최단거리 및 최단경로)
 public:
 	unordered_map<int, vector<pair<int, int>>> adjacencyList;
 	AreaInfo* indexArray[MAX]; // 지역번호 100000~999999
-	pair<int, int> parent_id[MAX]; // 부모번호와 최단거리
 	int distanceArray[MAX];
 	int countTreeVertex;
-	Status statusCheck[MAX];
+	int parent[MAX];
+	bool treeVertex[MAX];
 
 	Graph() {
 		countTreeVertex = 0;
+		for (int i = 0; i < MAX; i++) {
+			indexArray[i] = 0;
+			parent[i] = -1;
+		}
 	}
 
 	void insertVertex(int _id, string _name, bool _flood) {
@@ -52,9 +51,26 @@ public:
 	} // vertex 정보 저장 및 index 정보 저장
 
 	void connectVertex(int _fromID, int _toID, int _weight) {
-		adjacencyList[_fromID].push_back(make_pair(_weight, _toID));
-		adjacencyList[_toID].push_back(make_pair(_weight, _fromID));
-		// 무향그래프이므로 양쪽 모두 연결
+		int flag = true;
+		for (int i = 0; i < adjacencyList[_fromID].size(); i++) {
+			if (_toID == adjacencyList[_fromID][i].second) {
+				adjacencyList[_fromID][i].first = min(adjacencyList[_fromID][i].first, _weight);
+				flag = false;
+				break;
+			}
+		}
+		if (flag) {
+			adjacencyList[_fromID].push_back(make_pair(_weight, _toID));
+			adjacencyList[_toID].push_back(make_pair(_weight, _fromID));
+		}
+		else {
+			for (int i = 0; i < adjacencyList[_toID].size(); i++) {
+				if (_fromID == adjacencyList[_toID][i].second) {
+					adjacencyList[_toID][i].first = min(adjacencyList[_toID][i].first, _weight);
+				}
+			}
+		}
+		 //무향그래프이므로 양쪽 모두 연결
 	}
 
 	bool isFlooding(int _id) {
@@ -65,54 +81,53 @@ public:
 		return indexArray[_id];
 	}
 
-	pair<int, int>* dijkstra_A(int from_id, int to_id) { // 최단거리
+	void dijkstra_A(int from_id, int to_id) { // 최단거리
 		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; //weight, id
 		countTreeVertex = 0;
-
+	
 		for (int i = 0; i < MAX; i++) {
 			distanceArray[i] = INF;
-			statusCheck[i] = Status::UNSEEN;
+			parent[i] = -1;
+			treeVertex[i] = false;
 		}
-		statusCheck[from_id] = Status::TREE;
+
 		pq.push(make_pair(0, from_id));
 		distanceArray[from_id] = 0;
-		parent_id[from_id] = make_pair(-1, 0);
 
 		while (!pq.empty()) {
 			int distance = pq.top().first;
 			int here = pq.top().second;
 			pq.pop();
-			countTreeVertex++;
-			statusCheck[from_id] = Status::TREE;
-
-			if (here == to_id) {
-				return parent_id;
+			if (!treeVertex[here]) {
+				countTreeVertex++; //todo : 중복된 값 없는지 케이스 체크해보기
+				treeVertex[here] = true;
+			}
+			if (here == to_id || distance > MAX) {
+				return;
 			}
 
 			for (int i = 0; i < adjacencyList[here].size(); i++) {
-				int next = adjacencyList[here][i].second;
 				int weight = adjacencyList[here][i].first;
+				int next = adjacencyList[here][i].second;
 
-				if (distanceArray[next] > (distance + weight) && !isFlooding(next) && (statusCheck[next] != Status::TREE)) { // 거리가 update되면서 범람되지 않은 구역일때
+				if ( distanceArray[next] > (distance + weight) ) { // 거리가 update되면서 범람되지 않은 구역일때
 					distanceArray[next] = distance + weight;	  // decreaseKey
 					pq.push(make_pair(distanceArray[next], next));
-					parent_id[next] = make_pair(here, distanceArray[next]);
-					
+					parent[next] = here; // todo : parent 배열 초기화하고, 만약 -1이 아니라면 min 값 ID 넣어야할것
 				}
 			}
 		}
-		return parent_id;
+		return;
 	}
 
 
 	void query_A(int _fromID, int _toID) {
-		pair<int, int>* resultParentArray = dijkstra_A(_fromID, _toID);
-		int minimalDistance = resultParentArray[_toID].second;
-		if (minimalDistance > MAX || isFlooding(_fromID) || isFlooding(_toID)) {
+		dijkstra_A(_fromID, _toID);
+		int minimalDistance = distanceArray[_toID];
+		if (minimalDistance > MAX) {
 			cout << "None" << "\n";
 		}
 		else {
-
 			cout << countTreeVertex << " " << minimalDistance << " " << getVertex(_fromID)->areaName << " " << getVertex(_toID)->areaName << "\n";
 			/*출력형식: “N W SN DN” or “None”
 				N : 출발지부터 목적지까지 Tree vertex들의 set에 추가된 지역들의 수
@@ -123,23 +138,24 @@ public:
 	}
 
 	void query_B(int _fromID, int _toID) {
-		pair<int, int>* resultParentArray = dijkstra_A(_fromID, _toID);
-		int minimalDistance = resultParentArray[_toID].second;
-		if (minimalDistance > MAX || isFlooding(_fromID) || isFlooding(_toID)) {
+		dijkstra_A(_fromID, _toID);
+		int minimalDistance = distanceArray[_toID];
+		if (minimalDistance > MAX) {
 			cout << "None" << "\n";
 		}
 		else {
 			stack<int> st;
-
-			for (int i = _toID; i != -1; i = resultParentArray[i].first) {
+			cout << countTreeVertex << " ";
+			
+			for (int i = _toID ; i != -1; i = parent[i]) {
 				st.push(i);
 			}
 
-			cout << countTreeVertex << " ";
 			while (!st.empty()) {
 				cout << st.top() << " ";
 				st.pop();
 			}
+
 			cout << "\n";
 			/*출력형식: “N W SN DN” or “None”
 							N : 출발지부터 목적지까지 Tree vertex들의 set에 추가된 지역들의 수
@@ -153,8 +169,6 @@ int input_id, input_flooding, input_fromID, input_toID, input_weight;
 string queryName, input_name;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
 	cin >> N >> M >> Q;
 	Graph graph;
 
@@ -164,10 +178,17 @@ int main() {
 	}
 	for (int i = 0; i < M; i++) { // 지역 간의 도로 정보 입력 받기
 		cin >> input_fromID >> input_toID >> input_weight;
+		if (graph.getVertex(input_fromID)->isFlooding || graph.getVertex(input_toID)->isFlooding)
+			continue;
 		graph.connectVertex(input_fromID, input_toID, input_weight);
 	}
 	for (int i = 0; i < Q; i++) { // 질의 입력
 		cin >> queryName >> input_fromID >> input_toID;
+		if (graph.getVertex(input_fromID)->isFlooding || graph.getVertex(input_toID)->isFlooding){
+			cout << "None" << "\n";
+			continue;
+		}
+			
 		if (queryName == "A") { // 두 지역 간의 최단 거리 찾기
 			graph.query_A(input_fromID, input_toID);
 		}
